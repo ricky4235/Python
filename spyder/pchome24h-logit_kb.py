d30a53a64e8f685539111c3f0bffb1d0480759a6
ricky <ricky1229x@gmail.com>
2019-11-26 17:51:55 +0800

    20191126.c

--- spyder/pchome24h-logit_kb.py
+++ spyder/pchome24h-logit_kb.py
@@ -4,20 +4,14 @@ Created on Fri Nov 22 12:14:42 2019
 
 @author: 11004076
 """
-   
 import time
 import requests
 from bs4 import BeautifulSoup
 
 # 目標URL網址
-URL = "https://24h.pchome.com.tw/store/DCAHBC"
-       
-def generate_urls(url, start_page, end_page): #使用參數基底URL、開始和結束頁數來建立URL清單
-    urls = []   #爬蟲主程式建立的目標網址清單
-    for page in range(start_page, end_page+1):
-        urls.append(url.format(page))
-    return urls
-
+url = "https://24h.pchome.com.tw/store/DCAHBC"
+     
+  
 def get_resource(url):
     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
                "AppleWebKit/537.36 (KHTML, like Gecko)"
@@ -38,30 +32,24 @@ def get_goods(soup):
         goods.append(good)
     return goods
 
-def web_scraping_bot(urls):
-    all_goods = [["品名","價格"]]  #巢狀清單
-    page = 1
+def web_scraping_bot(url):
+    all_goods = [["品名","評價","樣本數","價格"]]  #巢狀清單
     
-    for url in urls:
-        print("抓取: 第" + str(page) + "頁 網路資料中...")
-        page = page + 1
-        r = get_resource(url)
-        if r.status_code == requests.codes.ok:
-            soup = parse_html(r.text)
-            goods = get_goods(soup)
-            all_goods = all_goods + goods
-            print("等待5秒鐘...")
-            if soup.find("li", class_="nexttxt disabled"):
-                break   #已經沒有下一頁
-            time.sleep(5) 
-        else:
-            print("HTTP請求錯誤...")
+    r = get_resource(url)
+    if r.status_code == requests.codes.ok:
+        soup = parse_html(r.text)
+        goods = get_goods(soup)
+        all_goods = all_goods + goods
+        print("等待5秒鐘...")
+        time.sleep(5)
+    else:
+        print("HTTP請求錯誤...")
 
     return all_goods
 
 if __name__ == "__main__":
-    urls = generate_urls(URL, 1, 1) 
-    print(urls)
-    goods = web_scraping_bot(urls) 
+    get_resource(url)
+    print(url)
+    goods = web_scraping_bot(url) 
     for good in goods:
         print(good)
