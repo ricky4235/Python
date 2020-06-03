75fa485382f4c49147a1fc228ef6b4b8fa43b376
ricky <ricky1229x@gmail.com>
2020-06-03 18:07:36 +0800

    20200603.c

--- 1_Crawler/PY/shoppe_Indonisia_detail.py
+++ 1_Crawler/PY/shoppe_Indonisia_detail.py
@@ -7,55 +7,84 @@ Created on Thu May 28 15:34:16 2020
 
 import pandas as pd
 import re, time, requests
+import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import csv
 
-#爬取點入網址
+
+def get_soup(url):
+    headers = {"user-agent": "Googlebot"}
+    res = requests.get(url, headers=headers)
+    return BeautifulSoup(res.text, 'lxml')
+
+# 爬取點入網址
 def FindLinks(url):
     linklist = []
-    driver = webdriver.Chrome()
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source)
-
-    links = soup.find_all('a', {'class':'BaseGridItem__content___3LORP'})
+    soup = get_soup(url)
+    links = soup.find_all("div",class_="col-xs-2-4 shopee-search-item-result__item")
     linklist = []
     for link in links:
-        k = link.get('href')
+        k = "https://id.xiapibuy.com/" + link.find("a").get('href')
         linklist.append(k)
     return linklist
 
-#爬取點入分頁資料
-def GetGoods(response):
-    soup2 = BeautifulSoup(response.text)
-    #url = soup2.find('link')['href']  #多餘的
-    df = pd.DataFrame(data = [{'TITLE':soup2.find('span', attrs={'itemprop':'name'}).text,
-                               'Price':soup2.find('span', attrs={'class':'has_promo_price'}), 
-                               'Activity Price':soup2.find('span',attrs={'class':'price'}).text }],
-                               columns = ['TITLE', 'Price', 'Activity Price']) 
-    return df
+# 爬取點入分頁資料
+def get_goods(url):
+    goods = []
+    rows = get_soup(url)
 
-#測試主畫面有無爬到各點入畫面網址
-urls = 'https://tw.search.mall.yahoo.com/search/mall/product?kw=%E6%98%86%E7%9B%88&p=%E6%98%86%E7%9B%88&cid=0&clv=0'
-FindLinks(urls)
+    for row in rows:
 
-#測試點入分頁有無爬到
-url = 'https://tw.mall.yahoo.com/item/GENIUS-%E6%98%86%E7%9B%88-DX-110-USB-%E7%86%B1%E5%8A%9B%E7%B4%85-%E6%9C%89%E7%B7%9A%E5%85%89%E5%AD%B8%E6%BB%91%E9%BC%A0-p0904196550128'
-resp = requests.get(url)
-GetGoods(resp)
+        try:
+            name = row.find('div', class_="qaNIZv").text
+        except:
+            name = None
 
-url = 'https://tw.mall.yahoo.com/item/Logitech%E7%BE%85%E6%8A%80-%E7%84%A1%E7%B7%9A%E6%BB%91%E9%BC%A0M235-%E7%81%B0%E3%80%90%E6%84%9B%E8%B2%B7%E3%80%91-p067085113817'
-resp = requests.get(url)
-GetGoods(resp)
+        try:
+            price = row.find('div', class_="_3n5NQx").text
+        except:
+            price = None
+            
+        try:
+            Original_price = row.find("div", class_="_3_ISdg").text
+        except:
+            Original_price = None
+            
+        try:
+            star = row.find('div', '_3Oj5_n _2z6cUg').text
+        except:
+            star = None
+            
+        try:
+            sold = row.find('div', '_22sp0A').text
+        except:
+            sold = None
+            
+        try:
+            stock = row.select_one('._1FzU2Y .items-center div+ div').text
+        except:
+            stock = None    
+        
+        
+        good= [name, price, Original_price, star, sold, stock]
+        goods.append(good)
+        
+    return goods[1]  #因為不知為何第[0]列都會出現一排None，只好取第[1]列
 
-urls = 'https://tw.search.mall.yahoo.com/search/mall/product?cid=hp&clv=0&kw=logitech%E7%84%A1%E7%B7%9A%E6%BB%91%E9%BC%A0&p=logitech%E7%84%A1%E7%B7%9A%E6%BB%91%E9%BC%A0'
-ndf = []
-for i in FindLinks(urls):
-    res = requests.get(i)
-    g = GetGoods(res)
-    ndf.append(g)
-    time.sleep(1)
-ndf
+# 爬取每一個點入頁面
+def scraping(urls):
+    ndf = []
+    for i in FindLinks(urls):
+        g = get_goods(i)
+        ndf.append(g)
+        time.sleep(0.5)
+        df_all = pd.DataFrame(ndf,columns = ["name", "price", "Original_price", "star", "sold", "stock"])
+    return df_all
 
-#合併資料
-df_all = pd.concat(ndf)
-df_all
+# 開始爬蟲
+if __name__ == "__main__":
+    urls = 'https://id.xiapibuy.com/search?attrId=14478&attrName=Merek&attrVal=genius&facet=12162&noCorrection=true&page=0'
+    print(scraping(urls))
+    
+    save_to_csv(goods, "Shoppe_Indonisia_detail.csv")
