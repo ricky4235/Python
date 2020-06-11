287366fe208b10477a001e8157a8d88754e7ce67
ricky <ricky1229x@gmail.com>
2020-06-11 18:04:20 +0800

    20200611.c

--- 1_Crawler/PY/Amazon_Rank_20200610.py
+++ 1_Crawler/PY/Amazon_Rank_20200610.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 22 12:14:42 2019
+Created on Tue Oct 15 16:03:33 2019
 
 @author: 11004076
 """
-   
-import time
+"""
+爬取各國亞馬遜單品排行榜頁面簡易商品資料_20200611完整版
+1. 使用F12頁面的headers取得解析網頁資料
+2. 先取得不同頁數的網址List
+3. 依序爬取網址List之商品資料
+"""
 import requests
 from bs4 import BeautifulSoup
-import pandas
+import time
 import csv
-
-# 目標URL網址
-url_no_pg = "https://www.amazon.com.br/gp/bestsellers/computers/16364919011/ref=pd_zg_hrsr_computers" #巴西
-#url_no_pg = "https://www.amazon.com/Best-Sellers-Computers-Accessories-Computer-Keyboards/zgbs/pc/12879431/ref=zg_bs_nav_pc_4_11036491" #美國
-#url_no_pg = "https://www.amazon.cn/gp/bestsellers/pc/888524051/ref=pd_zg_hrsr_pc" #中國
-#url_no_pg = "https://www.amazon.ae/gp/bestsellers/computers/12050385031/ref=zg_bs_nav_4_12050386031" #阿拉伯
-URL = url_no_pg +"&pg={0}" #加入換頁字串
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
        
 def generate_urls(url, start_page, end_page): #使用參數基底URL、開始和結束頁數來建立URL清單
     urls = []   #爬蟲主程式建立的目標網址清單
@@ -38,29 +37,28 @@ def get_goods(soup):
     goods = []
     rows = soup.find_all("li", class_="zg-item-immersion")
     #因為網頁常有空值，故需使用try except，不然會遇到None，就整個程式停掉
-    #下面會有兩種方法是因為之前不懂，就直接抓標籤，然後硬湊出來，還是把方法留下
+    
     for row in rows:  
         rank = row.find("span", class_="zg-badge-text").text #因為這不會有None，不需使用try except
+        
         try:
             #name = row.find("div", class_="p13n-sc-truncated").text  #不知為何這個抓出來都空白
             name = row.find("div", class_="a-section a-spacing-small").img["alt"]
         except:
             name = None
+            
         try:
             star = row.find("span", class_="a-icon-alt").text
-            #star = str(row.find("span", class_="a-icon-alt"))[25:28]
         except:
-            star = None    
+            star = None
+            
         try:
             reviews = row.find("a", class_="a-size-small a-link-normal").text
-            #下面兩行比較特別，先轉換成str，但位置都不同，所以得用find找">"和"<"的位置，再包在slicing中，等同取得">"到"<"之間的字串
-            #reviews_tag = str(row.find("a", class_="a-size-small a-link-normal"))
-            #reviews = reviews_tag[reviews_tag.find(">",1)+1 : reviews_tag.find("<",1)]
         except:
             reviews = None
+            
         try:         
             price = row.find("span", class_="p13n-sc-price").text
-            #price = str(row.find("span", class_="p13n-sc-price"))[28:33]
         except:
             price = None            
         
@@ -96,12 +94,34 @@ def save_to_csv(items, file):
             writer.writerow(item)
 
 if __name__ == "__main__":
+# 目標URL網址
+    #不同國家滑鼠
+    #url = "https://www.amazon.com/Best-Sellers-Computers-Accessories-Computer-Mice/zgbs/pc/11036491/ref=zg_bs_pg_2?_encoding=UTF8" #美國
+    #url = "https://www.amazon.cn/gp/bestsellers/pc/1454004071/ref=zg_bs_nav_pc_3_888467051" #中國
+    #url = "https://www.amazon.ae/gp/bestsellers/computers/12050386031/ref=zg_bs_nav_4_12050385031" #阿拉伯
+    #url = "https://www.amazon.com.br/gp/bestsellers/computers/16364917011/ref=zg_bs_nav_computers_4_16364919011" #巴西
+    #不同國家鍵盤
+    #url_no_pg = "https://www.amazon.com.br/gp/bestsellers/computers/16364919011/ref=pd_zg_hrsr_computers" #巴西
+    #url_no_pg = "https://www.amazon.com/Best-Sellers-Computers-Accessories-Computer-Keyboards/zgbs/pc/12879431/ref=zg_bs_nav_pc_4_11036491" #美國
+    #url_no_pg = "https://www.amazon.cn/gp/bestsellers/pc/888524051/ref=pd_zg_hrsr_pc" #中國
+    #url_no_pg = "https://www.amazon.ae/gp/bestsellers/computers/12050385031/ref=zg_bs_nav_4_12050386031" #阿拉伯
+    URL = url +"&pg={0}"  #加入換頁字串
+    
     urls = generate_urls(URL, 1, 3)  #得到1~3頁url的
     print(urls)
-    goods = web_scraping_bot(urls) 
-    df = pandas.DataFrame(goods)       #用dataframe列出
-    print(df)
-    #for good in goods:                #用list列出
-    #    print(good)
-    save_to_csv(goods, "Amazon_KB_Rank.csv")
     
+    goods = web_scraping_bot(urls)
+    df = pd.DataFrame(goods)       #用dataframe列出
+    print(df)
+    
+    save_to_csv(goods, "Amazon_Mouse_Rank.csv")
+    
+    """
+    資料分析部分
+    """
+    df.columns = ["rank", "name", "star", "reviews", "price"]  #重新命名欄位名稱
+    df_pivot = df.pivot_table(values = "name", columns = "star", aggfunc=np.count_nonzero)
+    print(df_pivot)
+
+    df_pivot.plot(kind = 'bar')
+    plt.show()
--- 1_Crawler/PY/untitled3.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 10 16:27:51 2020
-
-@author: 11004076
-"""
-
-$ pip install fuzzywuzzy
