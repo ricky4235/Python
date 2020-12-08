261a0dcd0ec40bc86ed3f6d59706ce91523b852b
ricky <ricky1229x@gmail.com>
2020-12-08 18:01:19 +0800

    20201208.c

--- 1_Crawler/PY/kg.py
+++ 1_Crawler/PY/kg.py
@@ -506,6 +506,7 @@ Xtrans = imputer.transform(X)
 
 print('KNNImputer after Missing: %d' % sum(np.isnan(Xtrans).flatten()))
 
+
 import numpy as np
 from sklearn.metrics import confusion_matrix
 y_pred = [0, 1, 0, 1]
@@ -541,25 +542,9 @@ def find_outliers_by_3segama(data,fea):
 
 import warnings
 warnings.filterwarnings('ignore')
-import itertools
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from sklearn import datasets
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB 
-from sklearn.ensemble import RandomForestClassifier
-from mlxtend.classifier import StackingClassifier
-from sklearn.model_selection import cross_val_score, train_test_split
-from mlxtend.plotting import plot_learning_curves
-from mlxtend.plotting import plot_decision_regions
 
 
 Dataframe(X,y) = ()
-
-
 
 clf1 = KNeighborsClassifier(n_neighbors=1)
 clf2 = RandomForestClassifier(random_state=1)
@@ -568,27 +553,35 @@ lr = LogisticRegression()
 sclf = StackingClassifier(classifiers=[clf1, clf2, clf3], 
                           meta_classifier=lr)
 
-
 label = ['KNN', 'Random Forest', 'Naive Bayes', 'Stacking Classifier']
 clf_list = [clf1, clf2, clf3, sclf]
-    
+
 fig = plt.figure(figsize=(10,8))
 gs = gridspec.GridSpec(2, 2)
 grid = itertools.product([0,1],repeat=2)
 
-
 clf_cv_mean = []
 clf_cv_std = []
 for clf, label, grd in zip(clf_list, label, grid):
-        
+
     scores = cross_val_score(clf, X, y, cv=5, scoring='accuracy')
     print("Accuracy: %.2f (+/- %.2f) [%s]" %(scores.mean(), scores.std(), label))
     clf_cv_mean.append(scores.mean())
     clf_cv_std.append(scores.std())
-        
+
     clf.fit(X, y)
     ax = plt.subplot(gs[grd[0], grd[1]])
     fig = plot_decision_regions(X=X, y=y, clf=clf)
     plt.title(label)
 
 plt.show()
+
+。
+
+
+
+4、格式轉換
+涉及對字符串類型的數值的處理，可以進行數據類型轉換和顆粒度變換。
+數據類型轉換：比如字符串轉換為時間格式；
+data1['Date'] = pd.to_datetime(data1['Date'])
+顆粒度變換：比如時間序列數據的平滑顯示（按天顯示變按月顯示）
